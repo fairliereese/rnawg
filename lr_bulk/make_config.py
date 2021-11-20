27c7936607f9fc2945b5584bfd50045d3a46cf76
fairliereese <fairliek@comcast.net>
2021-11-19 18:56:26 -0800

    fixed thing

--- lr_bulk/make_config.py
+++ lr_bulk/make_config.py
@@ -4,12 +4,13 @@ df = pd.read_csv('samples.txt', header=None, names=['name'])
 df['sample'] = df['name']
 df['platform'] = 'PacBio'
 df['fname'] = 'processing/'+df.name+'_labeled.sam'
+range = len(df.index)
 
 fname = 'talon/talon_config.csv'
 df.to_csv(fname, header=False, index=False)
 
 # more manageable chunks
-inds = [i for i in range(63) if i % 14 == 0]
+inds = [i for i in range(range) if i % 14 == 0]
 if len(df.index) not in inds:
     inds += [len(df.index)]
 
