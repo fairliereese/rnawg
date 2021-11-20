4e1f7625f5491e6b91d9869719e4a21c8700aa4b
fairliereese <fairliek@comcast.net>
2021-11-19 20:39:40 -0800

    added real biorep determination

--- lr_bulk/format_metadata.py
+++ lr_bulk/format_metadata.py
@@ -10,15 +10,31 @@ df.to_csv('biosample_exp_table.txt', index=False, header=False)
 
 # commands to make the file accession <-> sample ID
 df = pd.read_csv('metadata.tsv', sep='\t')
-df = df[['File accession', 'Experiment accession', 'Biosample term name', 'Technical replicate(s)']]
+df = df[['File accession', 'Experiment accession', 'Biosample term name', 'Technical replicate(s)', 'Biological replicate(s)']]
 map = pd.read_csv('biosamp_term_name_map.tsv', sep='\t', header=None, usecols=[1,2])
 map.columns = ['biosamp_name', 'new_biosamp_name']
 map.drop_duplicates(inplace=True)
 df = df.merge(map, how='left', left_on='Biosample term name', right_on='biosamp_name')
 df['new_biosamp_name'] = df.new_biosamp_name.str.lower()
 df['new_biosamp_name'] = df.new_biosamp_name.str.replace(' ', '_')
-df = df[['File accession', 'new_biosamp_name', 'Technical replicate(s)']]
-df['biosamp_name'] = df.new_biosamp_name+'_'+df['Technical replicate(s)']
+temp = df[['Experiment accession', 'biosamp_name', 'File accession']].groupby(['Experiment accession', 'biosamp_name']).count().reset_index()
+temp['biorep'] = temp.groupby('biosamp_name').cumcount()+1
+temp = temp[['Experiment accession', 'biorep']]
+temp.biorep = temp.biorep.astype(str)
+df = df.merge(temp, on='Experiment accession')
+# df['biosamp_name'] = df.new_biosamp_name+'_'+df['Technical replicate(s)']
+# df['biorep'] = df.groupby(['Experiment accession', 'biosamp_name']).cumcount() + 1
+# df['biorep'] = df.biorep.astype(str)
+df['hr'] = df.new_biosamp_name+'_'+df.biorep+'_'+df['Biological replicate(s)'].astype(str)
+
+# df = df.groupby(['Experiment accession', 'biosamp_name']).sum().reset_index()
+# df['biorep'] = df.sort_values(['Experiment accession', 'biosamp_name'],
+# 				ascending=[True, True])\
+# 				.groupby(['Experiment accession']) \
+# 				.cumcount() + 1
+
+
+df['beep'] = df.new_biosamp_name+'_'+df.biorep.astype(str)+'_'+df['Biological replicate(s)'].astype(str)
 df = df[['File accession', 'biosamp_name']]
 df.to_csv('file_to_hr.tsv', sep='\t', header=None, index=False)
 
