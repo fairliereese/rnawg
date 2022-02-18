3ebd7cd97982a530810e2a998eaf954a97de82ee
fairliereese <fairliek@comcast.net>
2022-02-18 15:51:30 -0800

    updating with new data

--- lr_bulk/README.md
+++ lr_bulk/README.md
@@ -1,7 +1,7 @@
 # Analysis of human ENCODE4 long read data
 
-* [ENCODE cart of human cell line data](https://www.encodeproject.org/carts/723c6f14-e68b-4480-8a61-704a15ac5c7a/)
-* [ENCODE cart of human tissue data](https://www.encodeproject.org/carts/26bd2879-329d-4168-98b9-6d132a1aad0f/)
+<!-- * [ENCODE cart of human cell line data](https://www.encodeproject.org/carts/723c6f14-e68b-4480-8a61-704a15ac5c7a/)
+* [ENCODE cart of human tissue data](https://www.encodeproject.org/carts/26bd2879-329d-4168-98b9-6d132a1aad0f/) -->
 * [ENCODE cart of all human data](https://www.encodeproject.org/carts/829d339c-913c-4773-8001-80130796a367/)
 * [Query for human data](https://www.encodeproject.org/search/?type=Experiment&control_type!=*&perturbed=false&assay_title=long+read+RNA-seq&lab.title=Ali+Mortazavi%2C+UCI&replicates.library.biosample.donor.organism.scientific_name=Homo+sapiens&award.rfa=ENCODE4&replicates.library.nucleic_acid_term_name=polyadenylated+mRNA&limit=all)
 
--- lr_bulk/format_metadata.py
+++ lr_bulk/format_metadata.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 # table of experiment <-> tissue
 df = pd.read_csv('metadata.tsv', sep='\t')
@@ -11,20 +12,29 @@ df.to_csv('biosample_exp_table.txt', index=False, header=False)
 # commands to make the file accession <-> sample ID
 df = pd.read_csv('metadata.tsv', sep='\t')
 df = df[['File accession', 'Experiment accession', 'Biosample term name', 'Biosample type', 'Technical replicate(s)', 'Biological replicate(s)']]
-map = pd.read_csv('biosamp_term_name_map.tsv', sep='\t', header=None, usecols=[1,2])
-map.columns = ['biosamp_name', 'new_biosamp_name']
+map = pd.read_csv('biosamp_term_name_map.tsv', sep='\t', header=None, usecols=[0,1,4])
+map.columns = ['Experiment accession', 'Biosample term name', 'new_biosamp_name']
 map.drop_duplicates(inplace=True)
-df = df.merge(map, how='left', left_on='Biosample term name', right_on='biosamp_name')
+df = df.merge(map, how='left', on=['Experiment accession', 'Biosample term name'])
 df['new_biosamp_name'] = df.new_biosamp_name.str.lower()
 df['new_biosamp_name'] = df.new_biosamp_name.str.replace(' ', '_')
-temp = df[['Experiment accession', 'biosamp_name', 'File accession']].groupby(['Experiment accession', 'biosamp_name']).count().reset_index()
-temp['biorep'] = temp.groupby('biosamp_name').cumcount()+1
+# temp = df[['Experiment accession', 'biosamp_name', 'File accession']].groupby(['Experiment accession', 'biosamp_name']).count().reset_index()
+# temp['biorep'] = temp.groupby('biosamp_name').cumcount()+1
+temp = df[['Experiment accession', 'new_biosamp_name', 'File accession']].groupby(['Experiment accession', 'new_biosamp_name']).count().reset_index()
+temp['biorep'] = temp.groupby('new_biosamp_name').cumcount()+1
+
 temp = temp[['Experiment accession', 'biorep']]
 temp.biorep = temp.biorep.astype(str)
 df = df.merge(temp, on='Experiment accession')
 df['techrep'] = df.groupby('Experiment accession').cumcount()+1
 df['hr'] = df.new_biosamp_name+'_'+df.biorep+'_'+df.techrep.astype(str)
 temp = df[['File accession', 'hr']]
+
+try:
+    os.rename('file_to_hr.tsv', 'file_to_hr_back.tsv')
+except:
+    pass
+
 temp.to_csv('file_to_hr.tsv', sep='\t', header=None, index=False)
 
 # save info about whether each dataset is a cell line or tissue
@@ -42,11 +52,16 @@ temp.to_csv('hr_to_biosample_type.tsv', sep='\t', index=False)
 
 # make the samples file
 try:
-    old_samples = pd.read_csv('samples.txt', header=None)
-    old_samples.columns = ['hr']
+    old_samples = pd.read_csv('file_to_hr_back.tsv', header=None, sep='\t')
+    old_samples.columns = ['file_id', 'hr']
 
-    # only include old samples
-    df = df.loc[~df.hr.isin(old_samples.hr.tolist())]
+    # # only include old files
+    # df = df.loc[~df.hr.isin(old_samples.hr.tolist())]
+    # only include new files
+    df = df.loc[~df['File accession'].isin(old_samples.file_id.tolist())]
+except:
+    pass
+
 
 df = df['hr']
 df.to_csv('samples.txt', header=None, index=None)
