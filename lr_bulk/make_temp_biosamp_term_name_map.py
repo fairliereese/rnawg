18bccdb33b84d40597c69930504750e3edeafab6
fairliereese <fairliek@comcast.net>
2022-04-12 09:38:46 -0700

    updated with rush brain data

--- lr_bulk/make_temp_biosamp_term_name_map.py
+++ lr_bulk/make_temp_biosamp_term_name_map.py
@@ -4,12 +4,21 @@ df = df[['Experiment accession', 'Biosample term name',
          'Biosample treatments', 'Biosample treatments duration']]
 df = df.drop_duplicates()
 
+print(df.head())
+
 # merge with preexising shorthand names
+# temp = pd.read_csv('biosamp_term_name_map.tsv', sep='\t', header=None,
+#                    names=['Experiment accession', 'Biosample term name',
+#                           'shorthand'])
 temp = pd.read_csv('biosamp_term_name_map.tsv', sep='\t', header=None,
+                   usecols=[0,1,4],
                    names=['Experiment accession', 'Biosample term name',
                           'shorthand'])
 temp.drop('Biosample term name', axis=1, inplace=True)
+print(temp.head())
 
 df = df.merge(temp, on='Experiment accession', how='left')
 df = df.sort_values(by='Biosample term name')
+# df = df.drop_duplicates()
+print(df.head())
 df.to_csv('temp_biosamp_term_name_map.tsv', sep='\t', index=False, header=None)
